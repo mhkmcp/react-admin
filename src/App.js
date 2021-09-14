1e6bd163be3aaf32e9cfb49f2f7d0a198ea8558c
mhkmcp <mhkmcp@yahoo.com>
2021-09-14 22:56:38 +0600

    added topbar-001

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       Hello
-      </header>
+    <div>
+     <Topbar />
     </div>
   );
 }
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './topbar.css'
+
+import {NotificationsNone, Language, Settings} from '@material-ui/icons';
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="topbarWrapper">
+                <div className="topbarLeft">
+                    <span className="logo">
+                        <span className="highlight">Lama</span>Admin
+                    </span>
+                </div>
+                <div className="topbarRight">
+                    <div className="topbarIconContainer">
+                        <NotificationsNone />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Language />
+                        <span className="topIconBadge">2</span>
+                    </div>
+                    <div className="topbarIconContainer">
+                        <Settings />
+                    </div>
+                    <img src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="Profile Picture" className="topAvatar" />
+                </div>
+            </div>
+        </div>
+    )
+}
