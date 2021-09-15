227f03a2dba77e2962a72369c492959a7bf023ab
mhkmcp <mhkmcp@yahoo.com>
2021-09-16 00:14:41 +0600

    chart-004 added

---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const userData = [
+    {
+      name: 'Jan',
+      "Active User": 4500,
+    },
+    {
+        name: 'Feb',
+        "Active User": 4300,
+    },
+    {
+        name: 'Mar',
+        "Active User": 3200,
+    },
+    {
+        name: 'Apr',
+        "Active User": 3500,
+    },
+    {
+        name: 'May',
+        "Active User": 4300,
+    },
+    {
+        name: 'Jun',
+        "Active User": 4000,
+    },
+    {
+        name: 'Jul',
+        "Active User": 3800,
+    },
+    {
+        name: 'Aug',
+        "Active User": 4700,
+    },
+    {
+        name: 'Sep',
+        "Active User": 5000,
+    },
+    {
+        name: 'Oct',
+        "Active User": 4300,
+    },
+    {
+        name: 'Nov',
+        "Active User": 6000,
+    },
+    {
+        name: 'Dec',
+        "Active User": 6500,
+    },
+
+  ];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import Chart from '../../components/chart/Chart';
 import Feature from '../../components/feature/Feature';
 
 import "./home.css";
+
+import { userData } from "../../dummyData";
 
 function Home() {
     return (
         <div className="home">
             <Feature />
+            <Chart data={userData} title="User Analytics" dataKey="Active User" grid="true"/>
         </div>
     )
 }
